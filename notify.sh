75565d00994408b4ce6630fee63b5f78eb4e39d7
Dylan <dylan@tritan.dev>
2023-09-23 20:08:54 -0400

    the cocker

--- notify.sh
+++ notify.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+TARGET_HOSTS=("core.as393577.net" "kc.ix.as393577.net" "kc02.ix.as393577.net" "ams.ix.as393577.net" "sj.ix.as393577.net")
 TARGET_URL="https://notify.kitty.software/bgp_down"
-FAILURE_MESSAGE="Core edge router is offline, fix it you big cocker fuck."
 
 send_notification() {
-  curl -d "$FAILURE_MESSAGE" "$TARGET_URL"
+  local hostname="$1"
+  local current_time="$(date)"
+  local message="Edge router $hostname is offline at $current_time. Fix it you small cock femboy fuck."
+  curl -d "$message" "$TARGET_URL"
 }
 
 while true; do
-  if ping -c 1 23.142.248.1 >/dev/null 2>&1; then
-    echo "Ping successful at $(date)"
-  else
-    echo "Ping failed at $(date)"
-    send_notification
-  fi
-  sleep 60 
+  for host in "${TARGET_HOSTS[@]}"; do
+    if ping -c 1 "$host" >/dev/null 2>&1; then
+      echo "Ping to $host successful at $(date)"
+    else
+      echo "Ping to $host failed at $(date)"
+      send_notification "$host"
+    fi
+  done
+  sleep 60
 done
